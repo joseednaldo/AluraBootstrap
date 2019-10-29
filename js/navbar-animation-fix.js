8e008e376990a5a7467325329d092603beb57811
EDNALDO santos <joseednaldo1983@gmail.com>
2019-10-29 10:12:27 -0300

    pasta js

---
+++ js/navbar-animation-fix.js
@@ -0,0 +1,7 @@
+$('#navbar-topCasaFina').on('show.bs.collapse',function(){
+	$('.topcasafina-banner').css('transform','translate(-50%,10%)');
+});
+
+$('#navbar-topCasaFina').on('hide.bs.collapse',function(){
+	$('.topcasafina-banner').css('transform','translate(-50%,-50%)');
+});
